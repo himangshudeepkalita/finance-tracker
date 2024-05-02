f4a0a90699314c561f9585e22924341973df718c
itzhimangshu <itzhimangshu@gmail.com>
2024-05-03 00:32:50 +0530

    creating functionality for form inputs

--- client/src/pages/dashboard/financial-record-form.jsx
+++ client/src/pages/dashboard/financial-record-form.jsx
@@ -1,3 +1,49 @@
+import { useState } from "react";
+
 export const FinancialRecordForm = () => {
-    return <div>Record From</div>;
+    const [description, setDescription] = useState<string>("");
+    const [amount, setAmount] = useState<string>("");
+    const [category, setCategory] = useState<string>("");
+    const [paymentMethod, setPaymentMethod] = useState<string>("");
+
+    const handleSubmit = () => {};
+
+  return (
+    <div className="form-container">
+      <form onSubmit={handleSubmit}>
+        <div className="form-field">
+          <label>Description:</label>
+          <input type="text" required className="input" />
+        </div>
+        <div className="form-field">
+          <label>Amount:</label>
+          <input type="number" required className="input" />
+        </div>
+        <div className="form-field">
+          <label>Category:</label>
+          <select required className="input">
+            <option value="">Select a Category</option>
+            <option value="Food">Food</option>
+            <option value="Rent">Rent</option>
+            <option value="Salary">Salary</option>
+            <option value="Utilities">Utilities</option>
+            <option value="Entertainment">Entertainment</option>
+            <option value="Other">Other</option>
+          </select>
+        </div>
+        <div className="form-field">
+          <label>Payment Method:</label>
+          <select required className="input">
+            <option value="">Select a Payment Method</option>
+            <option value="Credit Card">Credit Card</option>
+            <option value="Cash">Cash</option>
+            <option value="Bank Transfer">Bank Transfer</option>
+          </select>
+        </div>
+        <button type="submit" className="button">
+          Add Record
+        </button>
+      </form>
+    </div>
+  );
 };
