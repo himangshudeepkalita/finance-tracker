23c36fcf650d1326356837ca305f9966a84e662c
itzhimangshu <itzhimangshu@gmail.com>
2024-05-02 22:58:18 +0530

    setting up routes using react-router-dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,17 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
-  return <></>;
+  return (
+    <Router>
+      <div className="app-container">
+        <Routes>
+          <Route path="/" element={<h1>Dashboard</h1>} />
+          <Route path="/auth" element={<h1>SignIn</h1>} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
