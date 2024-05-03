a927cb5b2afa7ced32187262ac6b1eee23ae18e0
itzhimangshu <itzhimangshu@gmail.com>
2024-05-03 23:20:43 +0530

    defining routes for accessing financial records

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import financialRecordRouter from "../src/routes/financial-records.js";
 
 dotenv.config();
 
@@ -15,6 +16,8 @@ mongoose
   .connect(mongoURI)
   .then(() => console.log("CONNECTED TO MONGODB!"))
   .catch((err) => console.error("Failed to Connect to MongoDB:", err));
+
+app.use("/financial-records", financialRecordRouter);
 
 app.listen(port, () => {
     console.log(`Server Running on Port ${port}`);
---
+++ server/src/routes/financial-records.js
@@ -0,0 +1,6 @@
+import express from 'express';
+import FinancialRecordModel from '../schema/financial-record.js';
+
+const router = express.Router();
+
+export default router;
