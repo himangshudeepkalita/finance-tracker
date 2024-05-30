e4fc08d61020c49185a98eb77684aef3a60ab136
itzhimangshu <itzhimangshu@gmail.com>
2024-05-30 21:25:48 +0530

    fetching records based on user ID

--- server/src/routes/financial-records.js
+++ server/src/routes/financial-records.js
@@ -3,4 +3,17 @@ import FinancialRecordModel from '../schema/financial-record.js';
 
 const router = express.Router();
 
+router.get("/getAllByUserID/:userId", async (req, res) => {
+    try {
+        const userId = req.params.userId;
+        const records = await FinancialRecordModel.find({ userId: userId});
+        if(records.length === 0) {
+            return res.status(404).send("No records found for the user.")
+        }
+        res.status(200).send(records);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
 export default router;
