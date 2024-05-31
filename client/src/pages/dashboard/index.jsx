a33fbceb237ae1fb6fdaaf69d3e28e56e182a33b
itzhimangshu <itzhimangshu@gmail.com>
2024-06-01 00:28:27 +0530

    implementing a total monthly amount calculation

--- client/src/pages/dashboard/index.jsx
+++ client/src/pages/dashboard/index.jsx
@@ -1,14 +1,29 @@
 import { useUser } from "@clerk/clerk-react";
 import { FinancialRecordForm } from "./financial-record-form";
 import { FinancialRecordList } from "./financial-record-list";
+import "./financial-record.css";
+import { useFinancialRecords } from "../../contexts/financial-record-context";
+import { useMemo } from "react";
 
 export const Dashboard = () => {
-    const { user } = useUser();
-    return (
-        <div className="dashboard-container">
-            <h1> Welcome {user?.firstName}! Here Are Your Finances:</h1>
-            <FinancialRecordForm />
-            <FinancialRecordList />
-        </div>
-    );
+  const { user } = useUser();
+  const { records } = useFinancialRecords();
+
+  const totalMonthly = useMemo(() => {
+    let totalAmount = 0;
+    records.forEach((record) => {
+      totalAmount += record.amount;
+    });
+
+    return totalAmount;
+  }, [records]);
+
+  return (
+    <div className="dashboard-container">
+      <h1> Welcome {user?.firstName}! Here Are Your Finances:</h1>
+      <FinancialRecordForm />
+      <div>Total Monthly: ₹ {totalMonthly}</div>
+      <FinancialRecordList />
+    </div>
+  );
 };
