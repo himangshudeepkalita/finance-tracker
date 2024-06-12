be589ad934bbf4e7461fa19d2c2e731355f67849
itzhimangshu <itzhimangshu@gmail.com>
2024-06-12 23:39:59 +0530

    added vercel client side link in backend

--- server/src/index.js
+++ server/src/index.js
@@ -14,7 +14,8 @@ const port = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(cors({
-  origin: 'http://127.0.0.1:5173'
+  // origin: 'http://127.0.0.1:5173'
+  origin: 'https://finance-tracker-frontend-nine.vercel.app/'
 }));
 
 
